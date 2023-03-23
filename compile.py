31176e65f5b9e691b69a7b009698348de6490c9c
ParthP3 <parthneilpujari@gmail.com>
2023-03-23 14:04:00 +0530

    Changes in running instructions

--- compile.py
+++ compile.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 os.system("g++ -o Object/main GameEngine/main.cpp -lboost_system -lboost_thread -lboost_timer -pthread -lsfml-graphics -lsfml-window -lsfml-system ")
 os.system("g++ -o Object/client GameEngine/client.cpp -lboost_system -lboost_timer -lboost_thread -pthread")
